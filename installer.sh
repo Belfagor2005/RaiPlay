ea0ff66145e79595097171d8f763d5918b04f6ad
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-11-02 17:24:47 +0100

    Revert "2.9 v"
    This reverts commit 0797b1d561ea04427c2a188697e2550888629e53.

--- installer.sh
+++ installer.sh
@@ -2,8 +2,8 @@
 
 ## setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/Belfagor2005/RaiPlay/main/installer.sh -O - | /bin/sh
 ## Only This 2 lines to edit with new version ######
-version='1.9'
-changelog='\nAdd Download Manager - Notify'
+version='1.7'
+changelog='\nRai Play Final Version'
 ##############################################################
 TMPPATH=/tmp/RaiPlay-main
 FILEPATH=/tmp/main.tar.gz
